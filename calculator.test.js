e598b2efaaca765980b023d7ce3d9b11f7a860dd
aman-eth <aman.offficial@gmail.com>
2025-06-15 00:30:02 +0530

    test 4 : handle multiple comma-separated numbers

--- calculator.test.js
+++ calculator.test.js
@@ -11,3 +11,7 @@ test("returns number itself for single number input", () => {
 test("returns sum of two comma separated numbers", () => {
   expect(add("1,2")).toBe(3);
 });
+
+test("returns sum of multiple comma-separated numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
