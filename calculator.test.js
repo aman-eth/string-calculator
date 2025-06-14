c89948f4bc2b79d0f4d5efc2d8c314f62835f153
aman-eth <aman.offficial@gmail.com>
2025-06-15 00:26:41 +0530

    test 2 : handle single number input

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const { add } = require("./calculator");
 test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("returns number itself for single number input", () => {
+  expect(add("5")).toBe(5);
+});
